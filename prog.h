05713af5eb5ec1bbcf8e3257ca727f7b5ec5b2dc
GrigoriyAgelyarov <agelyarov15@mail.ru>
2026-05-07 16:30:44 +0300

    Update prog.h

--- prog.h
+++ prog.h
@@ -69,7 +69,6 @@ public:
 	VectorNormali() : A(0), B(0), C(0) {} //по умолчан
 	VectorNormali(double A_, double B_, double C_) : A(A_), B(B_), C(C_){} //инициал
 
-	double operator*(const Plane& pl) {};//скаляр
 	double operator*(const VectorNormali& vec) {};
 	VectorNormali operator^(const VectorNormali& pl) {};//векторн
 
